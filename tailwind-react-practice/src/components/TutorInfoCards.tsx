b320e2459ebc7810dc64c2d6a6cf36b9fdc5943c
MayHyeyeonKim <hkim7963@gmail.com>
2025-01-22 16:30:44 -0800

    updated tutorInfoCards

--- tailwind-react-practice/src/components/TutorInfoCards.tsx
+++ tailwind-react-practice/src/components/TutorInfoCards.tsx
@@ -1,4 +1,5 @@
 import dummyUser from '../assets/dummyUser.png';
+import dummyUser2 from '../assets/dummyUser2.png';
 
 type Tutor = {
     name: string;
@@ -15,12 +16,12 @@ const TutorDummy: Tutor[] = [
     {
         name: '이상해',
         introduction: '안녕하세요 저는 이상해입니다.',
-        image: dummyUser,
+        image: dummyUser2,
     },
     {
         name: '삼상해',
         introduction: '안녕하세요 저는 삼상해입니다.',
-        image: dummyUser,
+        image: dummyUser2,
     },
     {
         name: '사상해',
@@ -31,15 +32,26 @@ const TutorDummy: Tutor[] = [
 
 const TutorInfoCards = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {TutorDummy.map((tutor, index) => (
-                <div key={index} className='rounded-3xl bg-gray-200 p-4 mx-4 text-left overflow-hidden'>
-                    <div className='flex items-center'>
-                    <img src={tutor.image} alt={tutor.name} className="w-10 h-10 rounded-full object-cover" />
-                    <div className='p-5 space-y-2'>
-                        <div className='text-lg font-bold'>{tutor.name}</div>
-                        <div className='text-sm text-gray-500'>{tutor.introduction}</div>
-                    </div>
+                <div
+                    key={index}
+                    className="rounded-3xl bg-gray-200 mx-4 text-left overflow-hidden"
+                >
+                    <div className="flex flex-col sm: flex-row md: flex-row justify-center items-center w-full h-64">
+                        <img
+                            src={tutor.image}
+                            alt={tutor.name}
+                            className="w-full h-3/4 object-cover"
+                        />
+                        <div className="w-full h-1/4 space-y-2 flex flex-col justify-center items-center">
+                            <div className="text-lg font-bold">
+                                {tutor.name}
+                            </div>
+                            <div className="text-sm text-gray-500">
+                                {tutor.introduction}
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
